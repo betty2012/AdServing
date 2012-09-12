a3bd3984077b2c7635c1a7bc6020adf9921ab72d
thmarx <thmarx@gmx.net>
2012-09-12 15:59:11 +0200

    Change all servlets to asynccontext

--- modules/server/adserver/src/main/java/net/mad/ads/server/servlet/AdClick.java
+++ modules/server/adserver/src/main/java/net/mad/ads/server/servlet/AdClick.java
@@ -20,7 +20,11 @@ package net.mad.ads.server.servlet;
 import java.io.IOException;
 import java.util.UUID;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -39,10 +43,12 @@ import org.slf4j.LoggerFactory;
 /**
  * Servlet implementation class AdClick
  */
+@WebServlet(asyncSupported = true)
 public class AdClick extends HttpServlet {
-	
+	public static final int CALLBACK_TIMEOUT = 60000;
+
 	private static final Logger logger = LoggerFactory.getLogger(AdClick.class);
-	
+
 	private static final long serialVersionUID = 1L;
 
 	/**
@@ -57,7 +63,8 @@ public class AdClick extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
@@ -65,34 +72,90 @@ public class AdClick extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
-			IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
-	protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+	protected void processRequest(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException,
 			IOException {
-		String id = (String) request.getParameter("id");
-		
-		AdDefinition banner = RuntimeContext.getAdDB().getBanner(id);
-		
-		try {
-			AdContext context = AdContextListener.ADCONTEXT.get();
-			TrackEvent trackEvent = new ClickTrackEvent();
-			trackEvent.setBannerId(banner.getId());
-			trackEvent.setUser(context.getUserid());
-			trackEvent.setId(UUID.randomUUID().toString());
-			trackEvent.setTime(System.currentTimeMillis());
-			trackEvent.setIp(context.getIp());
-			
-//			RuntimeContext.getTrackService().track(trackEvent);
-//			RuntimeContext.clickLogger.click(banner.getId(), context.getUserid(), ""+trackEvent.getTime());
-			TrackingHelper.trackEvent(context, trackEvent);
-			TrackingHelper.trackClick(context, trackEvent);
-		} catch (Exception e) {
-			logger.error("", e);
-		}
-		
-		response.sendRedirect(banner.getTargetUrl());
+
+		// create the async context, otherwise getAsyncContext() will be null
+		final AsyncContext ctx = request.startAsync();
+
+		// set the timeout
+		ctx.setTimeout(CALLBACK_TIMEOUT);
+
+		// attach listener to respond to lifecycle events of this AsyncContext
+		ctx.addListener(new AsyncListener() {
+			/**
+			 * complete() has already been called on the async context, nothing
+			 * to do
+			 */
+			public void onComplete(AsyncEvent event) throws IOException {
+			}
+
+			/** timeout has occured in async task... handle it */
+			public void onTimeout(AsyncEvent event) throws IOException {
+				log("onTimeout called");
+				log(event.toString());
+				ctx.getResponse().getWriter().write("TIMEOUT");
+				ctx.complete();
+			}
+
+			/**
+			 * THIS NEVER GETS CALLED - error has occured in async task...
+			 * handle it
+			 */
+			public void onError(AsyncEvent event) throws IOException {
+				log("onError called");
+				log(event.toString());
+				ctx.getResponse().getWriter().write("ERROR");
+				ctx.complete();
+			}
+
+			/** async context has started, nothing to do */
+			public void onStartAsync(AsyncEvent event) throws IOException {
+			}
+		});
+
+		ctx.start(new Runnable() {
+
+			@Override
+			public void run() {
+				try {
+					String id = (String) request.getParameter("id");
+
+					AdDefinition banner = RuntimeContext.getAdDB()
+							.getBanner(id);
+
+					try {
+						AdContext context = AdContextListener.ADCONTEXT.get();
+						TrackEvent trackEvent = new ClickTrackEvent();
+						trackEvent.setBannerId(banner.getId());
+						trackEvent.setUser(context.getUserid());
+						trackEvent.setId(UUID.randomUUID().toString());
+						trackEvent.setTime(System.currentTimeMillis());
+						trackEvent.setIp(context.getIp());
+
+						// RuntimeContext.getTrackService().track(trackEvent);
+						// RuntimeContext.clickLogger.click(banner.getId(),
+						// context.getUserid(), ""+trackEvent.getTime());
+						TrackingHelper.trackEvent(context, trackEvent);
+						TrackingHelper.trackClick(context, trackEvent);
+					} catch (Exception e) {
+						logger.error("", e);
+					}
+
+					response.sendRedirect(banner.getTargetUrl());
+				} catch (IOException e) {
+					logger.error("", e);
+				} finally {
+					ctx.complete();
+				}
+			}
+		});
+
 	}
 }
--- modules/server/adserver/src/main/java/net/mad/ads/server/servlet/AdSelect.java
+++ modules/server/adserver/src/main/java/net/mad/ads/server/servlet/AdSelect.java
@@ -20,7 +20,11 @@ package net.mad.ads.server.servlet;
 import java.io.IOException;
 import java.util.UUID;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.AsyncEvent;
+import javax.servlet.AsyncListener;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -39,126 +43,206 @@ import net.mad.ads.server.utils.selection.AdProvider;
 /**
  * Servlet implementation class AdSelect
  */
+@WebServlet(asyncSupported = true)
 public class AdSelect extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	
-	
-	
+	public static final int CALLBACK_TIMEOUT = 60000;
+
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		processRequest(request, response);
 	}
-	
-	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		response.setContentType("text/javascript;charset=UTF-8");
-		
-		
-		
-		
-		try {
-			
-			AdContext context = AdContextListener.ADCONTEXT.get();
-			// Type
-//			String type = (String)request.getParameter(RequestHelper.type);
-//			if (type == null || type.equals("")) {
-//				type = "1";
-//			}
-			/*
-			 * Aus den restlichen Bannern eins auswählen
-			 * 
-			 * Aktuell wird dies zufällig gemacht!
+
+	private void processRequest(final HttpServletRequest request,
+			final HttpServletResponse response) throws ServletException, IOException {
+
+		// create the async context, otherwise getAsyncContext() will be null
+		final AsyncContext ctx = request.startAsync();
+
+		// set the timeout
+		ctx.setTimeout(CALLBACK_TIMEOUT);
+
+		// attach listener to respond to lifecycle events of this AsyncContext
+		ctx.addListener(new AsyncListener() {
+			/**
+			 * complete() has already been called on the async context, nothing
+			 * to do
 			 */
-			AdDefinition banner = AdProvider.getInstance().getBanner(context, request);
-			
-			StringBuilder sb = new StringBuilder();
-			if (banner != null) {
-				/*
-				 * Hier wird der Type des Banners verwendet und nicht der Typ der im
-				 * Request Übergeben wird, da bei der Auswahl des Banners evtl. ein
-				 * Fallback auf einen anderen BannerType erfolgen kann.
-				 * z.B. Flashbanner auf Imagebanner
-				 *  
-				 */
-//				if (banner.getType().getType().equals(ExternAdType.TYPE)) {
-//					sb.append(ExternAdDefinitionRenderer.getInstance().render(banner, request));
-//				} else if (banner.getType().getType().equals(ImageAdType.TYPE)) {
-//					sb.append(ImageAdDefinitionRenderer.getInstance().render(banner, request));
-//				} else if (banner.getType().getType().equals(FlashAdType.TYPE)) {
-//					sb.append(FlashAdDefinitionRenderer.getInstance().render(banner, request));
-//				} else if (banner.getType().getType().equals(ExpandableImageAdType.TYPE)) {
-//					sb.append(ExpandableImageAdDefinitionRenderer.getInstance().render(banner, request));
-//				}
-				AdDefinitionRenderer<AdDefinition> renderer = AdDefinitionRendererService.forType(banner.getType());
-				sb.append(renderer.render(banner, request));
-				
-				TrackEvent trackEvent = new ImpressionTrackEvent();
-				trackEvent.setBannerId(banner.getId());
-				trackEvent.setCampaign(banner.getCampaign() != null ? banner.getCampaign().getId() : "");
-				trackEvent.setUser(context.getUserid());
-				trackEvent.setId(UUID.randomUUID().toString());
-				trackEvent.setTime(System.currentTimeMillis());
-				trackEvent.setIp(context.getIp());
-				if (context.getSlot() != null) {
-					trackEvent.setSite(context.getSlot().getSite());
-				} else {
-					trackEvent.setSite("NONE_PAGE");
-				}
-				
-				
-				TrackingHelper.trackEvent(context, trackEvent);
-				
-				TrackingHelper.trackImpression(context, trackEvent);
-				
-				/*
-				 * 
-				 * Add request id and the ad id to the requested ad cache to rembemer which ad
-				 * was displayed at the current pageview.
-				 * This is used in the DuplicateBannerFilter to filter duplicate ads
-				 * 
-				 * Hier merken wir uns das Banner für diesen Request um später
-				 * im DuplicateBannerFilter die Information verwenden zu können
-				 * 
-				 * Als Request gelten alle Aufrufe, die durch den selben Pageview erzeugt werden
-				 * 
-				 * pv = pageview (all request from a single pageview)
-				 */
-				RuntimeContext.getRequestBanners().put("pv" + context.getRequestid() + "_" + banner.getId(), Boolean.TRUE);
-				/*
-				 * Hier merken wir uns, dass ein Benutzer das Banner schon gesehen hat.
-				 * Auf diese Art kann später z.B. geregelt werden, dass ein USER ein Banner maximal 5 mal sehen soll
-				 * 
-				 * TODO: hier muss noch die TimeToLife für das Cacheobjekte gesetzt werden
-				 * 
-				 * u = user
-				 */
-				RuntimeContext.getRequestBanners().put("u" + context.getUserid() + "_" + banner.getId(), Boolean.TRUE);
-				
-				/*
-				 * Damit wir später die passenden Banner für die Produkte anzeigen können, merken wir uns auch das Produkt
-				 * 
-				 * prod = product
-				 */
-				if (banner.isProduct()) {
-					RuntimeContext.getRequestBanners().put("prod" + context.getRequestid() + "_" + banner.getProduct(), Boolean.TRUE);
+			public void onComplete(AsyncEvent event) throws IOException {
+			}
+
+			/** timeout has occured in async task... handle it */
+			public void onTimeout(AsyncEvent event) throws IOException {
+				log("onTimeout called");
+				log(event.toString());
+				ctx.getResponse().getWriter().write("TIMEOUT");
+				ctx.complete();
+			}
+
+			/**
+			 * THIS NEVER GETS CALLED - error has occured in async task...
+			 * handle it
+			 */
+			public void onError(AsyncEvent event) throws IOException {
+				log("onError called");
+				log(event.toString());
+				ctx.getResponse().getWriter().write("ERROR");
+				ctx.complete();
+			}
+
+			/** async context has started, nothing to do */
+			public void onStartAsync(AsyncEvent event) throws IOException {
+			}
+		});
+
+		ctx.start(new Runnable() {
+
+			@Override
+			public void run() {
+				response.setContentType("text/javascript;charset=UTF-8");
+
+				try {
+
+					AdContext context = AdContextListener.ADCONTEXT.get();
+					// Type
+					// String type =
+					// (String)request.getParameter(RequestHelper.type);
+					// if (type == null || type.equals("")) {
+					// type = "1";
+					// }
+					/*
+					 * Aus den restlichen Bannern eins auswählen
+					 * 
+					 * Aktuell wird dies zufällig gemacht!
+					 */
+					AdDefinition banner = AdProvider.getInstance().getBanner(
+							context, request);
+
+					StringBuilder sb = new StringBuilder();
+					if (banner != null) {
+						/*
+						 * Hier wird der Type des Banners verwendet und nicht
+						 * der Typ der im Request Übergeben wird, da bei der
+						 * Auswahl des Banners evtl. ein Fallback auf einen
+						 * anderen BannerType erfolgen kann. z.B. Flashbanner
+						 * auf Imagebanner
+						 */
+						// if
+						// (banner.getType().getType().equals(ExternAdType.TYPE))
+						// {
+						// sb.append(ExternAdDefinitionRenderer.getInstance().render(banner,
+						// request));
+						// } else if
+						// (banner.getType().getType().equals(ImageAdType.TYPE))
+						// {
+						// sb.append(ImageAdDefinitionRenderer.getInstance().render(banner,
+						// request));
+						// } else if
+						// (banner.getType().getType().equals(FlashAdType.TYPE))
+						// {
+						// sb.append(FlashAdDefinitionRenderer.getInstance().render(banner,
+						// request));
+						// } else if
+						// (banner.getType().getType().equals(ExpandableImageAdType.TYPE))
+						// {
+						// sb.append(ExpandableImageAdDefinitionRenderer.getInstance().render(banner,
+						// request));
+						// }
+						AdDefinitionRenderer<AdDefinition> renderer = AdDefinitionRendererService
+								.forType(banner.getType());
+						sb.append(renderer.render(banner, request));
+
+						TrackEvent trackEvent = new ImpressionTrackEvent();
+						trackEvent.setBannerId(banner.getId());
+						trackEvent.setCampaign(banner.getCampaign() != null ? banner
+								.getCampaign().getId() : "");
+						trackEvent.setUser(context.getUserid());
+						trackEvent.setId(UUID.randomUUID().toString());
+						trackEvent.setTime(System.currentTimeMillis());
+						trackEvent.setIp(context.getIp());
+						if (context.getSlot() != null) {
+							trackEvent.setSite(context.getSlot().getSite());
+						} else {
+							trackEvent.setSite("NONE_PAGE");
+						}
+
+						TrackingHelper.trackEvent(context, trackEvent);
+
+						TrackingHelper.trackImpression(context, trackEvent);
+
+						/*
+						 * 
+						 * Add request id and the ad id to the requested ad
+						 * cache to rembemer which ad was displayed at the
+						 * current pageview. This is used in the
+						 * DuplicateBannerFilter to filter duplicate ads
+						 * 
+						 * Hier merken wir uns das Banner für diesen Request um
+						 * später im DuplicateBannerFilter die Information
+						 * verwenden zu können
+						 * 
+						 * Als Request gelten alle Aufrufe, die durch den selben
+						 * Pageview erzeugt werden
+						 * 
+						 * pv = pageview (all request from a single pageview)
+						 */
+						RuntimeContext.getRequestBanners().put(
+								"pv" + context.getRequestid() + "_"
+										+ banner.getId(), Boolean.TRUE);
+						/*
+						 * Hier merken wir uns, dass ein Benutzer das Banner
+						 * schon gesehen hat. Auf diese Art kann später z.B.
+						 * geregelt werden, dass ein USER ein Banner maximal 5
+						 * mal sehen soll
+						 * 
+						 * TODO: hier muss noch die TimeToLife für das
+						 * Cacheobjekte gesetzt werden
+						 * 
+						 * u = user
+						 */
+						RuntimeContext.getRequestBanners().put(
+								"u" + context.getUserid() + "_"
+										+ banner.getId(), Boolean.TRUE);
+
+						/*
+						 * Damit wir später die passenden Banner für die
+						 * Produkte anzeigen können, merken wir uns auch das
+						 * Produkt
+						 * 
+						 * prod = product
+						 */
+						if (banner.isProduct()) {
+							RuntimeContext
+									.getRequestBanners()
+									.put("prod" + context.getRequestid() + "_"
+											+ banner.getProduct(), Boolean.TRUE);
+						}
+					}
+
+					response.getWriter().write(sb.toString());
+
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					ctx.complete();
 				}
 			}
-			
-			
-			
-			response.getWriter().write(sb.toString());
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+
+		});
+
 	}
 }
