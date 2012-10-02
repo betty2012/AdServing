37e143d1e26656b049aa3d4f7d8c019e3eb5cf31
thmarx <thmarx@gmx.net>
2012-10-02 14:14:27 +0200

    add license info

--- modules/base/api/src/test/java/net/mad/ads/base/api/service/adserver/AdServerServiceTest.java
+++ modules/base/api/src/test/java/net/mad/ads/base/api/service/adserver/AdServerServiceTest.java
@@ -1,3 +1,20 @@
+/**
+ * Mad-Advertisement
+ * Copyright (C) 2011 Thorsten Marx <thmarx@gmx.net>
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package net.mad.ads.base.api.service.adserver;
 
 import static org.junit.Assert.*;
@@ -18,8 +35,8 @@ public class AdServerServiceTest {
 	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
-		provider = Provider.provider();
-	    provider.createAndPublishEndpoint("http://localhost:1234/adservice", new TestService());
+//		provider = Provider.provider();
+//	    provider.createAndPublishEndpoint("http://localhost:1234/adservice", new TestService());
 	}
 
 	@AfterClass
