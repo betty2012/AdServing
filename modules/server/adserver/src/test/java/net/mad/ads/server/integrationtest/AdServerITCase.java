615f5fdc603c19b417f2d422f3071d7fa0802c74
thmarx <thmarx@gmx.net>
2012-12-22 21:39:53 +0100

    add maven profile for running integration tests

---
+++ modules/server/adserver/src/test/java/net/mad/ads/server/integrationtest/AdServerITCase.java
@@ -0,0 +1,24 @@
+package net.mad.ads.server.integrationtest;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AdServerITCase {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		System.out.println("test");
+	}
+
+}
